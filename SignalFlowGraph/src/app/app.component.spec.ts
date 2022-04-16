316b547e032e10cb838e35a3a650a57f4c712dd7
LouaiZahran <louai0nasr@gmail.com>
2022-04-16 16:42:57 +0200

    Removed ProducerConsumer Garbage

--- SignalFlowGraph/src/app/app.component.spec.ts
+++ SignalFlowGraph/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProducerConsumer'`, () => {
+  it(`should have as title 'Signal Flow Graph'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProducerConsumer');
+    expect(app.title).toEqual('Signal Flow Graph');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ProducerConsumer app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Signal Flow Graph app is running!');
   });
 });
