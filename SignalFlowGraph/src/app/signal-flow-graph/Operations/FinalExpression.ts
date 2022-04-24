ba6faac63acdf29a21cb87d2c423d41b1fc9d68f
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2022-04-24 02:59:40 +0200

    wrong PARENTHESES BACK-END DONE -REALLY THIS TIME !!!!!! NO KIDDING FOR REAL

--- SignalFlowGraph/src/app/signal-flow-graph/Operations/FinalExpression.ts
+++ SignalFlowGraph/src/app/signal-flow-graph/Operations/FinalExpression.ts
@@ -29,7 +29,7 @@ export class FinalExpression {
   }
 
   private makeDenominator() {
-    this.denominator += "1 - ( ( "
+    this.denominator += "1 - ( "
 
     for (let i = 0; i < this.loopsGains.length; i++) {
       this.loopsGainsVals.push(this.makeGain(this.loopsGains[i]));
